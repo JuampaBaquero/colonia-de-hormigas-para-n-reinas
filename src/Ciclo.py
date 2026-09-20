5f55fb4890289da5fa3683c4c1ddee090394ca84
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-09-20 00:35:03 -0500

    es un ciclo de prueba

--- src/Ciclo.py
+++ src/Ciclo.py
@@ -1,8 +1,8 @@
-import time
-from functools import wraps #Para medir el tiempo 
+from conf import Coord 
 from Feromonas import Feromonas
 from Conflictos import Conflictos
 from Hormiga import Hormiga
+from Mediciones import Mediciones
 
 class Ciclo:
 
@@ -14,9 +14,52 @@ class Ciclo:
         self.feromonas: Feromonas = Feromonas(N, rho, tau_0)
         self.hormiga: Hormiga = Hormiga(N, self.conflictos, self.feromonas)
 
-    def ciclar(self, num_ciclos: int) -> None:
+    @Mediciones.medir_tiempo
+    def ciclar(self, num_ciclos: int) -> tuple[bool, int, list[Coord]]:
 
-        for _ in range(num_ciclos * self.N):    
-            conf_globales: int = self.hormiga.poner_reinas()
+        sol: list[Coord] = []
+        mejor_caso: int = int(self.N * (self.N + 1) / 2)
+        #sum_{i = 0}^N{N} = N(N+1) / 2 (gracias Gauss)
+        
+        for _ in range(num_ciclos * self.N):
+            conf_globales = self.hormiga.poner_reinas()
+
+            if conf_globales == 0:
+                sol = self.hormiga.reinas.copy()
+                return (True, 0, sol)
+
+            if conf_globales < mejor_caso:
+                mejor_caso = conf_globales
+                sol = self.hormiga.reinas.copy()
+
             self.hormiga.actualizar_feromonas(conf_globales)
-            self.conflictos.reiniciar()
+            self.hormiga.resetiar()
+
+        return (False, mejor_caso, sol)
+
+    @Mediciones.medir_tiempo
+    def ciclar_indef(self) -> tuple[int, list[Coord]]:
+
+        sol: list[Coord] = []
+        i: int = 1
+        j: int = 1
+        mejor_caso: int = int(self.N * (self.N + 1) / 2)
+        while True:
+            self.feromonas.reset()
+            i = 1
+            while i <= 10000:
+                conf_globales = self.hormiga.poner_reinas()
+
+                if conf_globales == 0:
+                    sol = self.hormiga.reinas.copy()
+                    return (j, sol)
+                
+                if conf_globales < mejor_caso:
+                    mejor_caso = conf_globales
+                    sol = self.hormiga.reinas.copy()
+
+                self.hormiga.actualizar_feromonas(conf_globales)
+                self.hormiga.resetiar()
+                i += 1
+
+            j += 1
