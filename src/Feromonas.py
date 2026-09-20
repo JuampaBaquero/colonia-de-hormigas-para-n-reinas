efa87d5a7d72aa4e2f4c25285c99ba3cda5b495f
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-09-20 00:35:39 -0500

    función para vaciar las feromonas cada tanto

--- src/Feromonas.py
+++ src/Feromonas.py
@@ -4,8 +4,10 @@ from conf import Coord
 class Feromonas:
 
     def __init__(self, N: int, rho: float, tau_0: float = 1.0) -> None:
+        self.N = N
         self.matriz: np.ndarray = np.full((N, N), tau_0)
         self.rho = rho
+        self.tau_0 = tau_0
 
     def evaporar_feromonas(self) -> None:
         self.matriz = self.matriz * self.rho #fockin python
@@ -13,3 +15,6 @@ class Feromonas:
     def actualizar_escogidas(self, lista: list[Coord], factor: float) -> None:
         for coord in lista:
             self.matriz[coord] += factor
+
+    def reset(self) -> None:
+        self.matriz = np.full((self.N,self.N), self.tau_0)
