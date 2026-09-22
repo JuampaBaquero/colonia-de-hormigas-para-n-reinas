2400d6a630e4950163c78091b992c1d04e1f1917
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-09-21 19:54:02 -0500

    Error super bobo de lógica corregido de N

--- src/Feromonas.py
+++ src/Feromonas.py
@@ -3,18 +3,20 @@ from conf import Coord
 
 class Feromonas:
 
-    def __init__(self, N: int, rho: float, tau_0: float = 1.0) -> None:
+    def __init__(self, N: int, rho: float, tau_min: float = 1.0) -> None:
         self.N = N
-        self.matriz: np.ndarray = np.full((N, N), tau_0)
+        self.matriz: np.ndarray = np.full((N, N), 1) #Llenamos la matríz con 1's 
         self.rho = rho
-        self.tau_0 = tau_0
+        self.tau_min = tau_min
 
     def evaporar_feromonas(self) -> None:
         self.matriz = self.matriz * self.rho #fockin python
+        menores_a_tau_min: np.ndarray = self.matriz < self.tau_min
+        self.matriz[menores_a_tau_min] = self.tau_min
 
     def actualizar_escogidas(self, lista: list[Coord], factor: float) -> None:
         for coord in lista:
             self.matriz[coord] += factor
 
     def reset(self) -> None:
-        self.matriz = np.full((self.N,self.N), self.tau_0)
+        self.matriz = np.full((self.N,self.N), self.tau_min)
