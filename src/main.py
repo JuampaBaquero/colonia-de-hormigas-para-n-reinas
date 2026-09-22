8258e5a44bb120e591221fc8350d49cb49510178
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-09-21 19:55:00 -0500

    creo que se me fue un espacio, no veo cambios

--- src/main.py
+++ src/main.py
@@ -1,41 +1,59 @@
-import time
-import numpy as np
-from conf import Coord
-from conf import condiciones_feromonas
-from Ciclo import Ciclo
+from Pruebas import Pruebas as p
+from conf import condiciones_feromonas, macro_condiciones
 import sys
 
-def imprimir_tablero(N: int, coordenadas: list[Coord]) -> None:
-    tablero: np.ndarray = np.full((N, N), '☐')
-    for pos in coordenadas:
-        tablero[pos] = '♛'
+def verificar_input(N: str, iteraciones: str, probar_varias: str = 'n') -> tuple[int, int, bool]:
+    while True:
+        try:
+            n: int = int(N)
+            if n < 4:
+                print('No puedes tener un N < 4...')
+                N = input('Ingresa otro valor de N: ')
+            else:
+                break
+        except ValueError:
+            print('N tiene que ser un número...')
+            N = input('Ingresa el valor de N: ')
 
-    print(tablero)
+    while True:
+        try:
+            iters: int = int(iteraciones)
+            if iters < 0:
+                print('Las iteraciones deben ser mayores o iguales a 0...')
+                iteraciones = input('Ingresa otro valor de iteraciones: ')
+            else:
+                break
+        except ValueError:
+            print('Iteraciones tiene que ser un número...')
+            iteraciones = input('Ingresa el valor de iteraciones: ')
 
-def prueba_ciclo(N: int, iteraciones: int) -> None:
-    ciclo: Ciclo = Ciclo(N, **condiciones_feromonas)
-    tiempo, (sol_optima, conflictos, coordenadas) = ciclo.ciclar(iteraciones) #type: ignore
+    probar: bool = (probar_varias == 's')
 
-    print(f'Las hormigas {'SI' if sol_optima else 'NO'} alcanzaron una solución óptima\n')
-    print(f'Hay {conflictos} conflictos')
-    print(f'El tablero es: ')
-    imprimir_tablero(N, coordenadas) # type: ignore
+    return n, iters, probar
 
-    print(f'La tin se demoró: {tiempo}')
+def main(N: int, iteraciones: int, probar_muchas: bool = False) -> None:
 
-def prueba_indef(N: int) -> None:
-    ciclo: Ciclo = Ciclo(N, **condiciones_feromonas)
-    tiempo, (iteraciones, coordenadas) = ciclo.ciclar_indef() #type: ignore
+    print(f'Corriendo algoritmo con estas condiciones:\n{condiciones_feromonas = }\n{macro_condiciones = }')
 
-    print(f'Las hormigas alcanzaron una solución óptima en {iteraciones} pasos')
-    print(f'El tablero es: ')
-    imprimir_tablero(N, coordenadas) # type: ignore
-
-    print(f'La tin se demoró: {tiempo}')
-        
-def main(N: int, iteraciones: int) -> None:
-    prueba_indef(N)
+    if probar_muchas:
+        if iteraciones == 0:
+            p.probar_n_tableros(N, iteraciones)
+    else:
+        if iteraciones == 0:
+            p.prueba_indef(N)
+        else:
+            p.prueba_def(N, iteraciones)
 
 if __name__ == "__main__":
     args: list[str] = sys.argv[1:]
-    main(int(args[0]), int(args[1]))
+
+    if len(args) > 3:
+        print('Muchos argumentos...')
+        print('Para usar el programa ejecuta uv run main.py N, ITERACIONES, s/n')
+        print('Si ITERACIONES es 0, ')
+
+    if len(args) == 2:
+        main(*verificar_input(*args))
+
+    if len(args) == 3:
+        main(*verificar_input(*args))
