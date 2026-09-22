3831a60a9eda2b34d9d5105905b85720404213c5
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-09-21 19:53:42 -0500

    Ahora almacena alpha y beta, que no los tenía. También distingue entre micro y macro parámetros

--- src/conf.py
+++ src/conf.py
@@ -1,6 +1,14 @@
-condiciones_feromonas: dict = {
-    'tau_0': 1.0,
-    'rho': 0.2
+condiciones_feromonas: dict[str, float] = {
+    'tau_min': 0.05,
+    'rho': 0.67,
+    'alpha': 1,
+    'beta': 3
+}
+
+macro_condiciones: dict[str, int] = {
+    'micro_iteraciones': 400,
+    'pruebas_por_tablero': 2,
+    'N_inicial': 4
 }
 
 type Coord = tuple[int, int]
